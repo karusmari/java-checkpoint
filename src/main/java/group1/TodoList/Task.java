e6cd81d9a54b28155288836d2fb095aa248e44e1
Maris Karu <maris.karu@gritlab.ax>
2025-09-24 14:08:37 +0300

    few more tasks updated

---
+++ src/main/java/group1/TodoList/Task.java
@@ -0,0 +1,32 @@
+package group1.TodoList;
+
+enum TaskStatus {
+    NEW, IN_PROGRESS, COMPLETED
+}
+
+public class Task {
+    private String description;
+    private TaskStatus status;
+
+    public Task(String description) {
+        this.description = description;
+        this.status = TaskStatus.NEW;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+}
